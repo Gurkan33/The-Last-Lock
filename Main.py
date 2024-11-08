50097c10d1fae7822321307c9c682af979c1b32e
Gurkan33 <gustav.olsen@skola.taby.se>
2024-11-08 21:25:06 +0100

    förslag på inledning med [simple_colors]!

--- Main.py
+++ Main.py
@@ -18,8 +18,13 @@ import simple_colors
 
 print(TextOchGubbar.rubrik)
 
-print("===================================================================")
-print(simple_colors.red("Welcome to The Last Lock!",["bold","underlined"]))
-print("Your now locked in a prison and have to fight your way out of it!")
+print(""),
 print("")
-player_Name = input
+print(simple_colors.red("Welcome to The Last Lock!",["bold","underlined"]))
+print(simple_colors.red("""Your now locked in a prison and have to fight your way out of it!
+    Open kests, Fight enemys, Avoid traps and more! """))
+print()
+player_Name = input(simple_colors.blue("Chose a name for you character: ",["bold"]))
+print(simple_colors.red("Ohh! Nice name " + simple_colors.blue(str(player_Name)) + "!"))
+print("")
+print(simple_colors.red("Now " + simple_colors.blue(str(player_Name)) + " its time to make your first move!"))
