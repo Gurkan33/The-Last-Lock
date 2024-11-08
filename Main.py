f1712b0997101b241c4d89cdd88c5bbd8528c0b6
Gurkan33 <gustav.olsen@skola.taby.se>
2024-11-08 20:47:38 +0100

    .

--- Main.py
+++ Main.py
@@ -1,4 +1,10 @@
 import TextOchGubbar
+import simple_colors 
 
 print(TextOchGubbar.rubrik)
-print("Hej")
+
+print("===================================================================")
+print(simple_colors.red("Welcome to The Last Lock!",["bold","underlined"]))
+print("Your now locked in a prison and have to fight your way out of it!")
+print("")
+player_Name = input
